b900c43a0d47d25d843af31f3ef4e98f76f59dfe
Jonathan Edgar <007j.edgar@gmail.com>
2017-07-19 11:35:13 -0500

    Initial Commit

---
+++ RemindWhere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemindWhere
+//
+//  Created by Jonathan Edgar on 7/19/17.
+//  Copyright © 2017 Jonathan Edgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
